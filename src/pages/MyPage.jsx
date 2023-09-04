547ebc7a4b35e27e2e6b999a8a97eec5ff39d788
10Hyuna <kimha105@naver.com>
2023-09-04 17:15:05 +0900

    [modify] router name

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import '../MyPage.css'; // 스타일 파일 임포트
 
-const MyPage = () => {
+const MyPage = ({ username, profileImage }) => {
   return (
-    <div>
-      <h1>My Page</h1>
+    <div className="user-profile-container">
+      <div className="profile-image">
+        <img src={profileImage} alt={`${username}'s Profile`} />
+      </div>
+      <h2>{username}</h2>
     </div>
   );
 };
