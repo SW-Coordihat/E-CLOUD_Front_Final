4a8d7feedaac7da617048def107faec229217a1b
yuseong <dog2655@naver.com>
2023-09-07 01:04:12 +0900

    [ADD] 채팅 형식으로 자료요청시 서버 전송후 이미지로드

---
+++ src/datas/dummyData.jsx
@@ -0,0 +1,21 @@
+export default function getImageDatas() {
+  let datas = [
+    {
+      src: "https://picsum.photos/500/300?img=1",
+    },
+    {
+      src: "https://picsum.photos/500/300?img=2",
+    },
+    {
+      src: "https://picsum.photos/500/300?img=3",
+    },
+    {
+      src: "https://picsum.photos/500/300?img=4",
+    },
+    {
+      src: "https://picsum.photos/500/300?img=5",
+    },
+  ];
+
+  return datas;
+}
